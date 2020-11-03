0f4aaeba25e02933f18dedd83fa79ff0185eabae
Prof. Leandro Vaguetti <prof.vaguetti@gmail.com>
2020-11-03 09:26:19 +0000

    add bulid

---
+++ public/build/precache-manifest.a1b8a9bfad082af0592ed0551df49671.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32baec2d6bd8ddbcd99bf72e96245987",
+    "url": "/index.html"
+  },
+  {
+    "revision": "956ef04258ef4dbaf00c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "441523b921b947076961",
+    "url": "/static/js/2.957e2439.chunk.js"
+  },
+  {
+    "revision": "956ef04258ef4dbaf00c",
+    "url": "/static/js/main.16890a8b.chunk.js"
+  },
+  {
+    "revision": "f67c199efb9f2c29f389",
+    "url": "/static/js/runtime-main.f294effb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
